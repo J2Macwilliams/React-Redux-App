c161def1be180960245884dc27e74ea339516e92
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-07 14:58:58 -0800

    Create .map of beers array.

--- src/components/List.js
+++ src/components/List.js
@@ -2,21 +2,31 @@ import React from "react";
 import { connect } from "react-redux";
 import { getBeers } from "../actions";
 
+import { Grid, Card } from '@material-ui/core';
 
 const BeerList = (props) => {
-console.log("Beer" , props);
-return (
-    <>
-    <button>Slainte'</button>
-    {props.isLoading && <div>spinner</div>}
-    {props.error && <div>{props.error.message}</div>}
-    
-    </>
-)
+    console.log("Beer", props);
+    return (
+        <>
+            <button>Slainte'</button>
+            {props.isLoading && <div>spinner</div>}
+            {props.error && <div>{props.error.message}</div>}
+            <Grid >
+
+                {props.beers.map(ale => (
+                    <Grid item key={ale.name}>
+                        <Card >
+                            {ale.name}
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
+        </>
+    )
 }
 const mapDispatchToProps = () => {
     return {
-       getBeers
+        getBeers
     }
 }
-export default connect(mapDispatchToProps , {})(BeerList);
+export default connect(mapDispatchToProps, {})(BeerList);
