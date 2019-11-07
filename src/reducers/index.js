137ed043ea384532ef92ef3e23aef176190304ff
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-07 13:51:17 -0800

    Create respective action cases in reducer.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import {LOADING , SUCCESS , FAILURE} from '../actions/index';
+import { LOADING, SUCCESS, FAILURE } from '../actions/index';
 
 const intialState = {
     names: [],
@@ -6,9 +6,28 @@ const intialState = {
     isLoading: false
 }
 
-export const reducer = (state = initialState , action) => {
+export const reducer = (state = initialState, action) => {
     console.log("reducer", action);
     switch (action.type) {
+        case LOADING:
+            return {
+                ...state,
+                error: null,
+                isLoading: true
+            }
+        case SUCCESS:
+            return {
+                ...state,
+                names: action.payload,
+                isLoading: false
+            }
+        case FAILURE:
+            return {
+                ...state,
+                names: [],
+                error: action.payload,
+                isLoading: false
+            }
         default:
             return state;
     }
