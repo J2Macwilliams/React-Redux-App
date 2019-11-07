4d59796efd5b092170d367ce566bf2d5c3ba9057
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-07 13:42:44 -0800

    Create action creators.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export const LOADING = "LOADING"
 export const SUCCESS = "SUCCESS"
 export const FAILURE = "FAILURE"
+
+export const loading = () => ({type: LOADING})
+export const success = (data) => ({type: SUCCESS, payload: data})
+export const failure = (data) => ({type: FAILURE, payload: data})
