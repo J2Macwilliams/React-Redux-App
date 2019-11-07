1249fc49331a568c661b29f84d1f4a7dd529e236
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-11-07 14:34:02 -0800

    Fix reducer path to index.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 import { applyMiddleware, createStore } from 'redux';
-import { reducer } from './reducer/reducer';
+import { reducer } from './reducer/index';
 import logger from "redux-logger";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
